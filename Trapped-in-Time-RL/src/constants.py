5758ac55db8b00a4cfd7cd122ca401198d02ee0c
jss5783 <jss5783@psu.edu>
2019-03-29 13:52:20 -0400

    Added missing constants.py

--- Trapped-in-Time-RL/src/constants.py
+++ Trapped-in-Time-RL/src/constants.py
@@ -1,13 +1,30 @@
 '''
 ---CHANGELOG---
+2019/03/27		(JSS5783)
+				Added colors. Added STATUS/MESSAGE.
+
 2019/03/22		(JSS5783)
 				constants.py created. Constants moved.
 '''
+
+import tcod
 
 #---CONSTANTS---
 SCREEN_WIDTH = 80		#for entire game; screen (game window) has multiple viewports (consoles)
 SCREEN_HEIGHT = 50
 MAP_WIDTH = SCREEN_WIDTH
 MAP_HEIGHT = SCREEN_HEIGHT - 5
+STATUS_WIDTH = 30
+STATUS_HEIGHT = SCREEN_HEIGHT - MAP_HEIGHT
+MESSAGE_WIDTH = SCREEN_WIDTH - STATUS_WIDTH
+MESSAGE_HEIGHT = STATUS_HEIGHT
 FPS_CAP = 20
 FONT = '../resources/terminal16x16_gs_ro.png'
+WHITE = tcod.Color(250,250,250)
+BLACK = tcod.Color(5,5,5)
+GRAY_DARK = tcod.Color(100,100,100)
+GRAY_LIGHT = tcod.Color(192,192,192)
+ORANGE_DARK = tcod.Color(165,100,15)
+ORANGE_LIGHT = tcod.Color(245,215,175)
+BLUE_DARK = tcod.Color(15,100,165)
+BLUE_LIGHT = tcod.Color(175,215,245)
